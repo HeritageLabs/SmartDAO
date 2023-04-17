b9576856414a264efb9b0a5ebb25cec49fb1582e
Temitope3665 <aroyewontope@gmail.com>
2023-04-17 15:19:47 +0100

    my feeds...

--- src/pages/feeds.tsx
+++ src/pages/feeds.tsx
@@ -1,19 +1,18 @@
 /* eslint-disable react/style-prop-object */
-import CustomButton from "../components/common/button";
+import AllDaos from "../components/daos/allDaos";
 import { FeedsLayout } from "../components/layouts";
+import AllProposals from "../components/proposal/allProposals";
 
 const Feeds = () => (
     <FeedsLayout>
       <p className="border-b-2 border-quaternary w-fit ml-8 font-gilroyMd text-quaternary">My Feeds</p>
       <div className="h-px bg-[#EEEEEE]" />
-      <div className="ml-8 pt-14 flex items-center w-2/6 justify-between">
-        <h1>All DAOs</h1>
-        <CustomButton bg="bg-bg">Create new DAO</CustomButton>
-        {/* <Filters /> */}
         <div className="mt-8">
-          {/* <AllDaos /> */}
+          <AllProposals />
         </div>
-      </div>
+        <div className="mt-8">
+          <AllDaos />
+        </div>
     </FeedsLayout>
 );
 
