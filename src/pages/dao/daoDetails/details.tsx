f82a1e53715ed8a8a00d2d34e8b68631cc524893
Temitope3665 <aroyewontope@gmail.com>
2023-05-14 08:49:58 +0100

    fix loading state

--- src/pages/dao/daoDetails/details.tsx
+++ src/pages/dao/daoDetails/details.tsx
@@ -26,57 +26,65 @@ const DaoDetails = () => {
           SideBar[0].value = (Number(res.balance) / 1e18).toFixed(2) + "AE";
           SideBar[1].value = Number(res.activeProposals).toString();
           SideBar[2].value = Number(res.proposals).toString();
+          setIsLoading(false)
         })
         .catch((error) => {
           console.log({ error });
           if (error.name == "NodeInvocationError") navigate("/daos");
+          setIsLoading(false);
         })
-        .finally(() => setIsLoading(false));
   }, [aeSdk]);
 
+  console.log(dao);
   return isLoading ? (
     <PageLoader />
   ) : (
     <FeedsLayout>
-      <DetailsNav setEnableCreateProposal={setEnableCreateProposal} dao={dao}>
-        {enableCreateProposal && <NewProposalTemp dao={dao.contractAddress} />}
-        <ProposalUpdate />
-        <div className="flex justify-between">
-          <div className="w-4/6 rounded-lg shadow-medium py-7 p-2">
-            <div className="flex justify-end mb-4 cursor-pointer">
-              <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd">
-                1W
-              </div>
-              <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
-                1M
-              </div>
-              <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
-                1Y
-              </div>
-              <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
-                ALL
-              </div>
-            </div>
-            <FundGraph />
-          </div>
-          <div className="w-4/12 ml-8">
-            {SideBar.map((val) => (
-              <div
-                className="px-4 py-5 shadow-medium rounded-lg cursor-pointer mb-7 hover:border hover:border-grey border border-white trans"
-                key={val.title}
-              >
-                <div className="flex justify-between text-sm">
-                  <p className="text-grey">{val.title}</p>
-                  <p className="text-quaternary font-gilroyMd">
-                    {val.percent}%
-                  </p>
+      {dao ? (
+        <DetailsNav setEnableCreateProposal={setEnableCreateProposal} dao={dao}>
+          {enableCreateProposal && (
+            <NewProposalTemp dao={dao.contractAddress} />
+          )}
+          <ProposalUpdate />
+          <div className="flex justify-between">
+            <div className="w-4/6 rounded-lg shadow-medium py-7 p-2">
+              <div className="flex justify-end mb-4 cursor-pointer">
+                <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd">
+                  1W
                 </div>
-                <h2 className="font-gilroyBold mt-2">{val.value}</h2>
+                <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
+                  1M
+                </div>
+                <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
+                  1Y
+                </div>
+                <div className="bg-lightBlue rounded p-2 text-[10px] font-gilroyMd ml-2">
+                  ALL
+                </div>
               </div>
-            ))}
+              <FundGraph />
+            </div>
+            <div className="w-4/12 ml-8">
+              {SideBar.map((val) => (
+                <div
+                  className="px-4 py-5 shadow-medium rounded-lg cursor-pointer mb-7 hover:border hover:border-grey border border-white trans"
+                  key={val.title}
+                >
+                  <div className="flex justify-between text-sm">
+                    <p className="text-grey">{val.title}</p>
+                    <p className="text-quaternary font-gilroyMd">
+                      {val.percent}%
+                    </p>
+                  </div>
+                  <h2 className="font-gilroyBold mt-2">{val.value}</h2>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </DetailsNav>
+        </DetailsNav>
+      ) : (
+        <PageLoader />
+      )}
     </FeedsLayout>
   );
 };
